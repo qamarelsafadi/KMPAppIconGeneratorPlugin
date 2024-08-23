00a70236c56135df8fb9fa5900f6de9532206e5c
mahmoud <mhmoudrhabib@gmail.com>
2024-08-23 17:31:24 +0300

    revert java version to version 11

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -36,10 +36,10 @@ publishing {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_19
-    targetCompatibility = JavaVersion.VERSION_19
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 kotlin {
-    jvmToolchain(19)
+    jvmToolchain(11)
 }
